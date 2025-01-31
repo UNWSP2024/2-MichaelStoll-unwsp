e228af1e22c50d2f6b58ffc33725d635bae7da9f
MichaelStoll-unwsp <mastoll@students.unwsp.edu>
2025-01-31 09:37:26 -0600

    Add files via upload

--- program_1.py
+++ program_1.py
@@ -1,6 +1,7 @@
-def personal_information():
-    print('...')
-
-
-# Line which calls the above function.
-personal_information()
+#title: personal info
+#author: michael stoll
+#date: 1/31/25
+print('Name: Michael Stoll')
+print('Address (fictional): 1234 5th st Duluth, Minnesota, 55287')
+print('Phone number (fictional):612-555-4342')
+print('College major: Currently undeclared, but working toward cybersecurity')
--- program_3.py
+++ program_3.py
@@ -1,8 +1,14 @@
-def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
-
-calculate_total_purchase()
+#title: store purchase
+#author: michael stoll
+#date: 1/31/25
+#I=item(ex: I_1 is item 1)
+I_1=float(input('Cost of item 1:'))
+I_2=float(input('Cost of item 2:'))
+I_3=float(input('Cost of item 3:'))
+I_4=float(input('Cost of item 4:'))
+I_5=float(input('Cost of item 5:'))
+subtotal=(I_1+I_2+I_3+I_4+I_5)
+print('Subtotal:', subtotal)
+print('sales tax: 7%')
+total=subtotal*1.07
+print('Total:', total)
